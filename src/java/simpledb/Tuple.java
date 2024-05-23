85cecdfaadaa91c2b72841e134ec9e7f11ee100b
def-Amore <jh2518252499@sjtu.edu.cn>
2024-05-23 14:17:13 +0800

    acmdb-lab4

---
+++ src/java/simpledb/Tuple.java
@@ -0,0 +1,186 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Tuple maintains information about the contents of a tuple. Tuples have a
+ * specified schema specified by a TupleDesc object and contain Field objects
+ * with the data for each field.
+ */
+public class Tuple implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    /**
+     *  类似于元组的schema信息
+     */
+    TupleDesc td;
+
+    /**
+     * 代表元组在disk的位置
+     */
+    RecordId rid;
+
+    /**
+     * 标示这一条记录的所有字段
+     */
+    CopyOnWriteArrayList<Field> fields;
+
+    /**
+     * Create a new tuple with the specified schema (type).
+     *
+     * @param td
+     *            the schema of this tuple. It must be a valid TupleDesc
+     *            instance with at least one field.
+     */
+    public Tuple(TupleDesc td) {
+        // some code goes here
+        // initialize the class
+        this.td = td;
+        this.fields = new CopyOnWriteArrayList<>();
+    }
+
+    /**
+     * @return The TupleDesc representing the schema of this tuple.
+     */
+    public TupleDesc getTupleDesc() {
+        // some code goes here
+        return this.td;
+
+    }
+
+    /**
+     * @return The RecordId representing the location of this tuple on disk. May
+     *         be null.
+     */
+    public RecordId getRecordId() {
+        // some code goes here
+        return this.rid;
+    }
+
+    /**
+     * Set the RecordId information for this tuple.
+     *
+     * @param rid
+     *            the new RecordId for this tuple.
+     */
+    public void setRecordId(RecordId rid) {
+        // some code goes here
+        this.rid = rid;
+    }
+
+    /**
+     * Change the value of the ith field of this tuple.
+     *
+     * @param i
+     *            index of the field to change. It must be a valid index.
+     * @param f
+     *            new value for the field.
+     */
+    public void setField(int i, Field f) {
+        // some code goes here
+        // if insert position is within range
+        if(i>=0 && i< fields.size()){
+            fields.set(i,f);
+        }
+        //else if it's on the edge of range
+        else if (i == fields.size() ){
+            fields.add(f);
+        }
+    }
+
+    /**
+     * @return the value of the ith field, or null if it has not been set.
+     *
+     * @param i
+     *            field index to return. Must be a valid index.
+     */
+    public Field getField(int i) {
+        // some code goes here
+        // judge the illegality first
+        if (fields==null || i>= fields.size()){
+            return null;
+        }
+        else{
+            return fields.get(i);
+        }
+    }
+
+    /**
+     * Returns the contents of this Tuple as a string. Note that to pass the
+     * system tests, the format needs to be as follows:
+     *
+     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     *
+     * where \t is any whitespace (except a newline)
+     */
+    public String toString() {
+        // some code goes here
+        StringBuilder strbdr = new StringBuilder();
+        Iterator<TupleDesc.TDItem> tdItems = this.td.iterator();
+        int index = 0;
+        while(tdItems.hasNext()){
+            tdItems.next();
+            strbdr.append(fields.get(index).toString());
+            strbdr.append(" ");
+            ++index;
+        }
+        return strbdr.toString();
+
+    }
+
+    /**
+     * @return
+     *        An iterator which iterates over all the fields of this tuple
+     * */
+    public Iterator<Field> fields()
+    {
+        // some code goes here
+        return fields.iterator();
+    }
+
+    /**
+     * reset the TupleDesc of thi tuple
+     * */
+    public void resetTupleDesc(TupleDesc td)
+    {
+        // some code goes here
+        this.td = td;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof  Tuple))
+        {
+            return false;
+        }
+
+        Tuple other = (Tuple) obj;
+        if (this.rid.equals(other.getRecordId()) &&
+                this.td.equals(other.getTupleDesc())) {
+            for (int i = 0; i < this.fields.size(); i++) {
+                if (!this.fields.get(i).equals(other.getField(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+    static public Tuple merge(Tuple tuple1, Tuple tuple2, TupleDesc schema1, TupleDesc schema2, TupleDesc joinSchema) {
+        Tuple mergedTuple =new Tuple(joinSchema);
+
+        for (int i = 0; i < schema1.numFields(); ++i) {
+            mergedTuple.setField(i, tuple1.getField(i));
+        }
+
+        for (int i = 0; i < schema2.numFields(); ++i){
+            mergedTuple.setField(i+ schema1.numFields(), tuple2.getField(i));
+        }
+        return mergedTuple;
+    }
+
+}
